cc8d97895db7d98be6c4c0fe9612830aad119762
Legopapurida <papuridalego@gmail.com>
2023-03-30 21:01:56 +0430

    Released Official

--- fastapi_authkit/__init__.py
+++ fastapi_authkit/__init__.py
@@ -2,6 +2,6 @@ from .base import OAuthApp
 from .settings import SETTINGS as AuthSetting
 from .core import providers as AuthProviders
 
-__version__ = "0.0.2"
+__version__ = "0.0.1"
 __author__ = "papuridalego@gmail.com"
 __all__ = ["OAuthApp", "AuthSetting", "AuthProviders"]
